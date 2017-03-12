9639647640471a416e8a1536eb83afdf4f570903
Kemati <kemati.morgan@mymona.uwi.edu>
2017-03-12 21:27:25 +0000

    updates5-sql

--- schema.sql
+++ schema.sql
@@ -3,5 +3,8 @@ CREATE TABLE users (
   id integer primary key autoincrement,
   firstname string not null,
   lastname string not null,
-  email string not null
+  age integer not null,
+  gender string not null,
+  biography string not null,
+  image file not null,
 );
